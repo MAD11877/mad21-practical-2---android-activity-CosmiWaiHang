dffd70a86e3de9472ac34648fdabcd32102b8ea8
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-05-11 17:21:53 +0800

    Test1

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.madpractical;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
